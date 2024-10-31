46bfc965969c3c91bba6a2159b5b3d81eaf41b31
yassinalmach <yasssineelmach0@gmail.com>
2024-10-31 18:25:03 +0100

    register hander func added

--- handlers/api/login.go
+++ handlers/api/login.go
@@ -1,10 +1,36 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"forum/controller"
+	"forum/models"
+	"net/http"
+)
+
+type resp struct {
+	Err  string `json:"error,omitempty"`
+	Data any    `json:"data,omitempty"`
+}
+
+func responseJSON(w http.ResponseWriter, code int, resp resp) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(resp)
+}
 
 func RegisterUser(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		responseJSON(w, 400, resp{Err: err.Error()})
+		return
+	}
 
+	if err := controller.AddUser(user.Email, user.Username, user.Password); err != nil {
+		responseJSON(w, 400, resp{Err: err.Error()})
+		return
+	}
 }
+
 func LoginUser(w http.ResponseWriter, r *http.Request) {
 
 }
