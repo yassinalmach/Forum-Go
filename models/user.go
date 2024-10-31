48a78421f44d61cbb547d5cec8d7b27f1bd1f29a
yassinalmach <yasssineelmach0@gmail.com>
2024-10-31 15:23:53 +0100

    add user & session struct

--- models/user.go
+++ models/user.go
@@ -1,1 +1,17 @@
 package models
+
+import "time"
+
+type User struct {
+	Id       int    `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Session struct {
+	Id        string    `json:"id"`
+	UserId    int64     `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
